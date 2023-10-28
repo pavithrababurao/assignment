ae95fdf233872b3768071b75338f4a88a1c3ed53
pavithrababurao <117432224+pavithrababurao@users.noreply.github.com>
2023-10-28 18:52:13 +0530

    Add files via upload

---
+++ RomanToInteger.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class RomanToInteger {
+    public static int romanToInt(String s) {
+        int result = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            int currValue = 0;
+
+            switch (c) {
+                case 'I':
+                    currValue = 1;
+                    break;
+                case 'V':
+                    currValue = 5;
+                    break;
+                case 'X':
+                    currValue = 10;
+                    break;
+                case 'L':
+                    currValue = 50;
+                    break;
+                case 'C':
+                    currValue = 100;
+                    break;
+                case 'D':
+                    currValue = 500;
+                    break;
+                case 'M':
+                    currValue = 1000;
+                    break;
+            }
+
+            if (currValue < prevValue) {
+                result -= currValue;
+            } else {
+                result += currValue;
+            }
+
+            prevValue = currValue;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a Roman numeral: ");
+        String romanNumeral = scanner.nextLine().toUpperCase();
+
+        int intValue = romanToInt(romanNumeral);
+        System.out.println("The integer value of " + romanNumeral + " is: " + intValue);
+
+        scanner.close();
+    }
+}
