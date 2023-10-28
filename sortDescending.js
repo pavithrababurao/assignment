34e21bcce7df60d59db529a5c7d1a3a5620b4b94
pavithrababurao <117432224+pavithrababurao@users.noreply.github.com>
2023-10-28 19:20:25 +0530

    Add files via upload

---
+++ sortDescending.js
@@ -0,0 +1,20 @@
+function sortDescending(arr) {
+  const n = arr.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] < arr[j + 1]) {
+        // Swap arr[j] and arr[j + 1]
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+}
+
+const numbers = [5, 2, 9, 1, 5, 6];
+
+bubbleSortDescending(numbers);
+
+console.log(numbers); 
